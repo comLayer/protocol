9a8e35bcfe05e2c428769ee68b9bedf9a8706a4a
MrZigha <gagnieux.virgil@proton.me>
2024-09-23 18:58:04 +0200

    PublicKeyRegistry done
    * feat(register jey): first poc of user address/key/algo mapping
    
    * setup Hardhat and add initial tests
    
    * Fix(PublicKeyRegistry): mismatch algorithms
    
    * allow extending a list of supported encryption algorithms
    
    Signed-off-by: Anton Ptashnik <iavtomator@gmail.com>
    
    * allow excluding an encryption algo from supported list
    
    Signed-off-by: Anton Ptashnik <iavtomator@gmail.com>
    
    * add tests for unregister, getPubKey calls and operation rate limit
    
    Signed-off-by: Anton Ptashnik <iavtomator@gmail.com>
    
    * refactor a register func and remove an extra error def EmptyEncryptionAlgorithm
    
    * remove an unused func
    
    * add a test to verify a user cannot violate op rate limit
    
    ---------
    
    Signed-off-by: Anton Ptashnik <iavtomator@gmail.com>
    Co-authored-by: loic.fregeac <loic.fregeac@meria.com>
    Co-authored-by: Anton Ptashnik <iavtomator@gmail.com>

---
+++ contract/contracts/PublicKeyRegistry.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title PublicKeyRegistry
+ * @dev A contract for storing and managing public keys linked to Ethereum addresses in comLayer protocol.
+ * Users can register or unregister their public keys and retrieve public keys of others.
+ */
+contract PublicKeyRegistry {
+
+    /// @dev Structure to store public key information.
+    struct PublicKeyInfo {
+        bytes publicKey;              // User's public key (binary data)
+        string encryptionAlgorithm;   // Encryption algorithm (RSA : 0x00)
+        uint256 lastRegisteredAt;     // Timestamp of the last registration to prevent spamming
+    }
+
+    /// @dev Mapping to link an Ethereum address to its public key information.
+    mapping(address => PublicKeyInfo) private publicKeys;
+
+    /// @dev Mapping to link an Ethereum address to its public key information.
+    mapping(string => bool) private supportedEncryptionAlgorithms;
+
+    /// account who deployed the contract
+    address private owner;
+
+    /// @dev Minimum time required between registering/unregistering operations to prevent spam.
+    uint256 constant RATE_LIMIT_TIME = 1 minutes;
+
+    /// @notice Emitted when a user registers a public key.
+    /// @param user The address of the user registering the public key.
+    /// @param publicKey The public key being registered.
+    /// @param encryptionAlgorithm The encryption algorithm used for the public key.
+    event PublicKeyRegistered(address indexed user, bytes publicKey, string encryptionAlgorithm);
+
+    /// @notice Emitted when a user unregisters their public key.
+    /// @param user The address of the user unregistering the public key.
+    event PublicKeyUnregistered(address indexed user);
+
+    // Custom errors
+    error PublicKeyTooShort();
+    error UnsupportedEncryptionAlgorithm();
+    error RateLimitExceeded();
+    error NoPublicKeyRegistered();
+    error AccessDenied();
+
+    constructor() {
+        supportedEncryptionAlgorithms["RSA"] = true;
+        owner = msg.sender;
+    }
+
+    /**
+     * @notice Registers a public key and associated encryption algorithm for the caller.
+     * @dev Public key and encryption algorithm must pass basic validation. Rate limit enforced.
+     * @param _publicKey The public key to be registered (in bytes format).
+     * @param _encryptionAlgorithm The encryption algorithm used for the public key.
+     */
+    function register(bytes calldata _publicKey, string calldata _encryptionAlgorithm) external {
+        _validateKey(_publicKey, _encryptionAlgorithm);
+
+        publicKeys[msg.sender] = PublicKeyInfo({
+            publicKey: _publicKey,
+            encryptionAlgorithm: _encryptionAlgorithm,
+            lastRegisteredAt: block.timestamp
+        });
+
+        emit PublicKeyRegistered(msg.sender, _publicKey, _encryptionAlgorithm);
+    }
+
+    /**
+     * @notice Unregisters the caller's public key.
+     * @dev Reverts if no public key is registered for the caller. Rate limit enforced.
+     */
+    function unregister() external {
+        if (bytes(publicKeys[msg.sender].publicKey).length == 0) revert NoPublicKeyRegistered();
+
+        // Rate limiting to prevent spamming
+        if (block.timestamp < publicKeys[msg.sender].lastRegisteredAt + RATE_LIMIT_TIME) {
+            revert RateLimitExceeded();
+        }
+
+        delete publicKeys[msg.sender];
+
+        emit PublicKeyUnregistered(msg.sender);
+    }
+
+    /**
+     * @notice Retrieves the public key and encryption algorithm for a given user address.
+     * @dev Reverts if no public key is found for the specified address.
+     * @param _user The address whose public key information is being requested.
+     * @return publicKey The public key associated with the user.
+     * @return encryptionAlgorithm The encryption algorithm associated with the user's public key.
+     */
+    function getPubKey(address _user) external view returns (bytes memory publicKey, string memory encryptionAlgorithm) {
+        if (bytes(publicKeys[_user].publicKey).length == 0) revert NoPublicKeyRegistered();
+
+        PublicKeyInfo storage info = publicKeys[_user];
+        return (info.publicKey, info.encryptionAlgorithm);
+    }
+
+    /**
+     * @notice Checks if a public key is registered for the given address.
+     * @param _user The address to check for a registered public key.
+     * @return isAddressRegistered A boolean indicating if the public key exists.
+     */
+    function isRegistered(address _user) external view returns (bool isAddressRegistered) {
+        return bytes(publicKeys[_user].publicKey).length > 0;
+    }
+
+    /**
+     * @notice Adds new algorithm allowing to register new keys with
+     * @param encryptionAlgorithm new algorithm to add
+     */
+    function addEncryptionAlgorithm(string calldata encryptionAlgorithm) external {
+        if (msg.sender != owner) {
+            revert AccessDenied();
+        }
+        supportedEncryptionAlgorithms[encryptionAlgorithm] = true;
+    }
+
+    /**
+     * @notice Removes a specified algorithm restricting new key registration
+     * @param encryptionAlgorithm an algorithm to remove
+     */
+    function removeEncryptionAlgorithm(string calldata encryptionAlgorithm) external {
+        if (msg.sender != owner) {
+            revert AccessDenied();
+        }
+        delete supportedEncryptionAlgorithms[encryptionAlgorithm];
+    }
+
+    /**
+     * @dev Validate a provided key agains the specified encryption algorithm and op rate limit
+     */
+    function _validateKey(bytes calldata _publicKey, string calldata _encryptionAlgorithm) internal view {
+        if (_publicKey.length < 32) revert PublicKeyTooShort();
+
+        if (!supportedEncryptionAlgorithms[_encryptionAlgorithm]) {
+            revert UnsupportedEncryptionAlgorithm();
+        }
+
+        // Rate limiting to prevent spamming
+        if (block.timestamp < publicKeys[msg.sender].lastRegisteredAt + RATE_LIMIT_TIME) {
+            revert RateLimitExceeded();
+        }
+    }
+}
---
+++ contract/test/PublicKeyRegistry.js
@@ -0,0 +1,151 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+describe.only("PublicKeyRegistry", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployContractFixture() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await ethers.getSigners();
+    const PublicKeyRegistry = await ethers.getContractFactory("PublicKeyRegistry");
+    const contract = await PublicKeyRegistry.deploy();
+
+    return { contract, owner, otherAccount };
+  }
+
+  it("Deployment smoke", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const PublicKeyRegistry = await ethers.getContractFactory("PublicKeyRegistry");
+    const contract = await PublicKeyRegistry.deploy();
+
+    expect(await contract.isRegistered(owner)).to.equal(false);
+  });
+
+  it("Should register/unregister a supported key when requested", async function () {
+    const { contract, owner } = await loadFixture(deployContractFixture);
+    
+    const key = "0x"+"a0".repeat(36);
+    const algo = "RSA"
+    await expect(contract.register(key,algo))
+      .to.emit(contract, "PublicKeyRegistered").withArgs(owner, key, algo);
+    expect(await contract.isRegistered(owner)).to.equal(true);
+    expect(await contract.getPubKey(owner)).to.include.members([key, algo]);
+
+    const timeOneMinAfter = (await time.latest()) + 61;
+    await time.increaseTo(timeOneMinAfter);
+
+    await expect(contract.unregister())
+      .to.emit(contract, "PublicKeyUnregistered").withArgs(owner);
+  });
+
+  it("Should revert registeration when allowed wrong key/algo passed", async function () {
+    const { contract, owner } = await loadFixture(deployContractFixture);
+    
+    const ok_key = "0x"+"a0".repeat(36);
+    const ok_algo = "RSA"
+    const wrong_key = "0x11";
+    const wrong_algo = "SOME"
+
+    await expect(contract.register(ok_key, wrong_algo))
+      .to.be.revertedWithCustomError(contract, "UnsupportedEncryptionAlgorithm");
+    await expect(contract.register(wrong_key, ok_algo))
+    .to.be.revertedWithCustomError(contract, "PublicKeyTooShort");
+    expect(await contract.isRegistered(owner)).to.equal(false);
+    await expect(contract.getPubKey(owner))
+    .to.be.revertedWithCustomError(contract, "NoPublicKeyRegistered");
+  });
+
+  it("Should be allowed to register keys with new algo once the algo introduced", async function () {
+    const { contract, owner } = await loadFixture(deployContractFixture);
+    
+    const key = "0x"+"a0".repeat(36);
+    const newAlgo = "newalgo"
+
+    await expect(contract.register(key, newAlgo))
+      .to.be.revertedWithCustomError(contract, "UnsupportedEncryptionAlgorithm");
+      
+    await contract.addEncryptionAlgorithm(newAlgo);
+
+    await expect(contract.register(key, newAlgo))
+    .to.emit(contract, "PublicKeyRegistered").withArgs(owner, key, newAlgo);
+  });
+
+  it("Should deny registering keys with an algo once the algo is removed", async function () {
+    const { contract, owner, otherAccount } = await loadFixture(deployContractFixture);
+    
+    const key = "0x"+"a0".repeat(36);
+    const newAlgo = "newalgo"
+
+    await contract.addEncryptionAlgorithm(newAlgo);
+
+    const callAsOwner = contract;
+    const callAsAccount1 = callAsOwner;
+    const callAsAccount2 = contract.connect(otherAccount);
+
+    await expect(callAsAccount1.register(key, newAlgo))
+    .to.emit(callAsAccount1, "PublicKeyRegistered").withArgs(owner, key, newAlgo);
+
+    await callAsOwner.removeEncryptionAlgorithm(newAlgo);
+
+    await expect(callAsAccount2.register(key, newAlgo))
+    .to.be.revertedWithCustomError(callAsAccount2, "UnsupportedEncryptionAlgorithm");
+    expect(await contract.isRegistered(otherAccount)).to.equal(false);
+  });
+
+  it("Should restrict new algo intro to contract owner only", async function () {
+    const { contract, owner, otherAccount} = await loadFixture(deployContractFixture);
+    const newAlgo = "newalgo"
+
+    const callAsOwner = contract;
+    const callAsNonOwner = contract.connect(otherAccount);
+    await expect(callAsNonOwner.addEncryptionAlgorithm(newAlgo))
+      .to.be.revertedWithCustomError(contract, "AccessDenied");
+
+    await expect(callAsOwner.addEncryptionAlgorithm(newAlgo)).to.be.ok;
+  });
+
+  it("Should restrict register operation rate for a user to 1 per min", async function () {
+    const { contract, owner } = await loadFixture(deployContractFixture);
+    
+    const key = "0x"+"a0".repeat(36);
+    const anotherKey = "0x"+"b0".repeat(36);
+    const algo = "RSA"
+    await expect(contract.register(key,algo))
+      .to.emit(contract, "PublicKeyRegistered").withArgs(owner, key, algo);
+    
+    await expect(contract.register(key,algo))
+      .to.be.revertedWithCustomError(contract, "RateLimitExceeded");
+
+    const nowTimestamp = await time.latest()
+    const oneMinLaterTimestamp = nowTimestamp + 60;
+
+    for (var sec of [15, 30, 45, 55]){
+      const timestamp = nowTimestamp + sec;
+      await time.increaseTo(timestamp);
+      await expect(contract.register(anotherKey, algo))
+        .to.be.revertedWithCustomError(contract, "RateLimitExceeded");
+    }
+
+    await time.increaseTo(oneMinLaterTimestamp);
+    await expect(contract.register(anotherKey, algo))
+    .to.emit(contract, "PublicKeyRegistered").withArgs(owner, anotherKey, algo);
+  });
+
+  it("Should restrict a user from violating operation rate limit by repeating register/unregister calls", async function () {
+    const { contract, owner } = await loadFixture(deployContractFixture);
+    
+    const key = "0x"+"a0".repeat(36);
+    const algo = "RSA"
+    await expect(contract.register(key,algo))
+      .to.emit(contract, "PublicKeyRegistered").withArgs(owner, key, algo);
+    
+    await expect(contract.unregister())
+      .to.be.revertedWithCustomError(contract, "RateLimitExceeded");
+  });
+});
